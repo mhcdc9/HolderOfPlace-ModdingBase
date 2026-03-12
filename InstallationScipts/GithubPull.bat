30b3966c1ae6ce9ddfb409cad10d6e742fcf2de4
Michael <160648273+mhcdc9@users.noreply.github.com>
2026-03-12 10:26:36 -0400

    Added unzip failsafes to githubPull.bat

--- InstallationScipts/GithubPull.bat
+++ InstallationScipts/GithubPull.bat
@@ -1,7 +1,10 @@
 @echo off
 setlocal EnableExtensions EnableDelayedExpansion
 
-set TO_UPDATE=0
+set UPDATE_MODE=0
+::0 -> Don't Update
+::1 -> Unknown Version (Update)
+::2 -> Old Version (Update)
 
 if not exist currentCatalog.txt (
 	echo Missing: currentCatalog.txt. Creating default txt...
@@ -9,16 +12,17 @@ if not exist currentCatalog.txt (
 	echo StreamingAssets 0 >> currentCatalog.txt
 	echo Dependencies 0 >> currentCatalog.txt
 )
-
-choice /C YN /M "Connect to GitHub and search for updates?
-if %errorlevel% EQU 2 (
-	echo Download the required files manually and then run the installer.
-	pause
-	exit /b
+if "%1" neq "skip" (
+	choice /C YN /M "Connect to GitHub and search for updates?
+	if !errorlevel! EQU 2 (
+		echo Download the required files manually and then run the installer.
+		pause
+		exit /b
+	)
 )
 
+::Fetch catalog from latest release
 echo Fetching Catalog...
-
 for /f "tokens=1,* delims=:" %%A in ('curl -ks https://api.github.com/repos/mhcdc9/HolderOfPlace-ModdingBase/releases/latest ^| findstr "browser_download_url"') do (
 	set url=%%B
 	if not "!url:catalog.txt=!"=="!url!" (
@@ -33,44 +37,83 @@ if not exist catalog.txt (
 	exit /b
 )
 
+::Find which files to download and download them
 echo Checking files...
-
 for /f "tokens=1,2" %%A in (catalog.txt) do (
-	set TO_UPDATE=0
+	set TO_UPDATE=1
 	for /f "tokens=1,2" %%a in (currentCatalog.txt) do (
 		if %%A==%%a (
+			set TO_UPDATE=0
 			if not %%B == %%b (
-				set TO_UPDATE=1
-				echo %%A: Updating from [%%B]...
-				if %%A==Installer (
-					echo A new Updater is required! Please download a new one from the GitHub repository:
-					echo https://github.com/mhcdc9/HolderOfPlace-ModdingBase
-					pause
-					exit
-				)
-				echo Downloading %%B...
-				curl -OL %%B
-				if %%A==StreamingAssets (
-					echo Unzipping [StreamingAssets.zip]...
-					call :UnZipFile "%~dp0" "StreamingAssets.zip"
-					del /Q "StreamingAssets.zip"
-				)
-				if %%A==Dependencies (
-					echo Unzipping [Dependencies.zip]...
-					call :UnZipFile "%~dp0" "Dependencies.zip"
-					del /Q "Dependencies.zip"
-				)
+				set TO_UPDATE=2
 			)
 		)
 	)
 	if !TO_UPDATE!==0 (
 	echo %%A: Up To Date
+	) else ( if !TO_UPDATE!==1 ( echo %%A: Unknown Version. Updating...
+		) else ( 
+			echo %%A: Old Version. Updating...
+			if %%A==Installer (
+			echo A new Updater is required! Please download a new one from the GitHub repository:
+			echo https://github.com/mhcdc9/HolderOfPlace-ModdingBase
+			pause
+			exit
+		)
+		)
+		echo Downloading %%B...
+		curl -OL %%B
+		if %%A==StreamingAssets (
+			echo Unzipping [StreamingAssets.zip]...
+			::Delete an old version of streamingassets
+			del /Q "StreamingAssets" 2>nul
+			call :UnZipFile "%~dp0" "%~dp0StreamingAssets.zip"
+		)
+		if %%A==Dependencies (
+			echo Unzipping [Dependencies.zip]...
+			::Delete an old version of dependencies
+			del /Q "Dependencies" 2>nul
+			call :UnZipFile "%~dp0" "%~dp0Dependencies.zip"
+		)
 	)
 )
-echo Replacing currentCatalog... 
-move /Y catalog.txt currentCatalog.txt
-echo Download Complete. Please run the installer
+
+::Failsafe in case unzip files does not work
+set NEED_TO_UNZIP=0
+if not exist Dependencies (
+	set /a NEED_TO_UNZIP+=1
+	echo [Dependencies.zip] needs to be unzipped
+)
+if not exist StreamingAssets (
+	set /a NEED_TO_UNZIP+=1
+	echo [StreamingAssets.zip] needs to be unzipped
+)
+if %NEED_TO_UNZIP% neq 0 (
+	echo Unzipping failed. Please unzip the %NEED_TO_UNZIP% file^(s^) above.
+	set NEED_TO_UNZIP=0
+	pause
+)
+set NEED_TO_UNZIP=0
+if not exist Dependencies (
+	set /a NEED_TO_UNZIP+=1
+	echo [Dependencies] still not extracted.
+)
+if not exist StreamingAssets (
+	set /a NEED_TO_UNZIP+=1
+	echo [StreamingAssets] still not extracted.
+)
+if %NEED_TO_UNZIP% neq 0 (
+	echo Files remain unzipped. Refusing to record changes.
+	endlocal
+	exit /b
+)
+
+::Everything is complete. Recording changes to avoid double-downloading
+echo Recording changes in currentCatalog... 
+move /Y catalog.txt currentCatalog.txt>nul
+echo Download Complete. Ready to install.
 pause
+endlocal
 exit /b
 
 :UnZipFile <ExtractTo> <newzipfile>
